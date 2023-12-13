6aed9c3663c4774d15df35116e93c94bcc8d0416
MediaMagnet <nathanec@gmail.com>
2023-12-13 15:38:11 -0600

    initial

---
+++ bin/scyther.dart
@@ -0,0 +1,5 @@
+import 'package:scyther/scyther.dart' as scyther;
+
+void main(List<String> arguments) {
+  print('Hello world: ${scyther.calculate()}!');
+}
---
+++ test/scyther_test.dart
@@ -0,0 +1,8 @@
+import 'package:scyther/scyther.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
